9f7cb3ba3a56ddb86ca25ac62852e7021143349c
Alvaro Martin <alvaro@MacBook-Pro-de-Alvaro.local>
2015-10-31 14:54:48 +0100

    Function Squares defined

--- django_eb/buscador/tests.py
+++ django_eb/buscador/tests.py
@@ -1,3 +1,7 @@
 from django.test import TestCase
 
 # Create your tests here.
+def squares(number):
+	print("please enter your number here: ")
+	squares = number ** 2
+	print("this is your number squared: " + squares)
